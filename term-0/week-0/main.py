df0d615bee0787ee88f6002b691d08154dfe4149
Seagram Kern <280533298+seagramkern8-dev@users.noreply.github.com>
2026-04-30 17:24:46 -0700

    Create main.py
    first Python file

---
+++ term-0/week-0/main.py
@@ -0,0 +1,11 @@
+print("Maestro starts June 1, 2026.")
+print("This is my Python prework space.")
+print("My goal is to build consistency before day one.")
+
+name = "Seagram"
+program = "Maestro"
+focus = "Python, problem-solving, and software development"
+
+print(name)
+print(program)
+print(focus)
